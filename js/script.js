c68ca1e31e4ff5464c224d4bbc82973abde7bce9
unknown <mohamadrezakurniawan@gmail.com>
2022-03-17 04:49:45 +0700

    kertas gunting batu

---
+++ js/script.js
@@ -0,0 +1,70 @@
+class Start {
+    constructor() {
+        this.playerName = 'PLAYER',
+            this.botName = 'BOT',
+            this.playerOption,
+            this.botOption,
+            this.winner = "";
+    }
+    set setBotOption(option) {
+        this.botOption = option;
+    }
+
+    get setBotoption() {
+        return this.botOption;
+    }
+
+    set setPlayerOption(option) {
+        this.playerOption = option;
+    }
+
+    get setPlayerOption() {
+        return this.playerOption;
+    }
+
+    botBrain() {
+        const option = ["🖐️", "✌️", "✊"];
+        const bot = option[Math.floor(Math.random() * option.length)];
+        return bot;
+    }
+
+    winCalculation() {
+        if (this.botOption == "🖐️" && this.playerOption == "✌️") {
+            return this.winner = this.playerName;
+        } else if (this.botOption == "🖐️" && this.playerOption == "✊") {
+            return this.winner = this.botName;
+        } else if (this.botOption == "✌️" && this.playerOption == "🖐️") {
+            return this.winner = this.botName;
+        } else if (this.botOption == "✌️" && this.playerOption == "✊") {
+            return this.winner = this.playerName;
+        } else if (this.botOption == "✊" && this.playerOption == "🖐️") {
+            return this.winner = this.playerName;
+        } else if (this.botOption == "✊" && this.playerOption == "✌️") {
+            return this.winner = this.botName;
+        } else {
+            return this.winner = "SERI";
+        }
+    }
+
+    matchResult() {
+        if (this.winner != "SERI") {
+            return `${this.winner} MENANG `;
+        } else {
+            return `WAH ${this.winner}, TIDAK ADA YANG MENANG`;
+        }
+    }
+
+}
+
+function pickOption(params) {
+    const start = new Start();
+    start.setPlayerOption = params;
+    start.setBotOption = start.botBrain();
+    start.winCalculation();
+
+    const inGame = document.getElementById("inGame");
+    const result = document.getElementById("result");
+
+    inGame.textContent = `${start.playerOption} VS ${start.botOption}`;
+    result.textContent = start.matchResult();
+}
